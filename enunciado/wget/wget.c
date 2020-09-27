fd8d63198f1571a2fd96b159f0b7f5bb88316a3c
FredyArroyave12 <FredyArroyave@udea.edu.co>
2020-09-27 10:22:35 -0500

    ...

---
+++ enunciado/wget/wget.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("wgrep: searchterm [file ...]\n");
+        exit(1);
+    }
+
+    if (argc == 2)
+    {
+        char buffer[1000];
+        while (fgets(buffer, 1000, stdin))
+        {
+            if (strstr(buffer, argv[1]) != NULL)
+            {
+                printf("%s", buffer);
+            }
+        }
+    }
+    else
+    {
+
+        for (int i = 2; i < argc; i++)
+        {
+
+            char *line = NULL;
+            size_t lenght = 0;
+            ssize_t read;
+        
+            FILE *fp = fopen(argv[i], "r");
+            if (fp == NULL)
+            {
+                printf("wgrep: cannot open file\n");
+                exit(1);
+            }
+
+            while ((read = getline(&line, &lenght, fp)) != -1)
+            {
+
+                if (strstr(line, argv[1]) != NULL)
+                {
+                    printf("%s", line);
+                }
+            }
+            fclose(fp);
+        }
+    }
+
+    return 0;
